8068db4131aba2f2957a7328d003d6a38a0b9761
jcolladosp <jocolsan@inf.upv.es>
2015-10-01 17:47:29 +0200

    Logica funcionando

--- app/src/main/java/es/jcolladosp/daviscope/PreguntaActivity.java
+++ app/src/main/java/es/jcolladosp/daviscope/PreguntaActivity.java
@@ -1,19 +1,34 @@
 package es.jcolladosp.daviscope;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import es.jcolladosp.daviscope.Util.BaseActivity;
 
-public class PreguntaActivity extends BaseActivity {
+public class PreguntaActivity extends BaseActivity implements View.OnClickListener {
+
+    Button aceptar;
+    EditText edpregunta;
+    public String pregunta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pregunta);
+
+
+        setListeners();
+        findViews();
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +50,35 @@ public class PreguntaActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+    private void setListeners() {
+        aceptar = (Button) findViewById(R.id.btAceptar);
+        aceptar.setOnClickListener(this);
+
+         }
+    private void findViews(){
+        edpregunta = (EditText) findViewById(R.id.edPregunta);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btAceptar) {
+
+            pregunta = edpregunta.getText().toString();
+
+            Intent i = new Intent(getApplicationContext(), ResultadoActivity.class);
+            i.putExtra("pregunta", pregunta);
+            startActivity(i);
+        }
+
     }
 }
--- app/src/main/java/es/jcolladosp/daviscope/ResultadoActivity.java
+++ app/src/main/java/es/jcolladosp/daviscope/ResultadoActivity.java
@@ -1,16 +1,44 @@
 package es.jcolladosp.daviscope;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ResultadoActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Random;
+
+import es.jcolladosp.daviscope.Util.BaseActivity;
+
+public class ResultadoActivity extends BaseActivity implements View.OnClickListener {
+    Button volver;
+    Button compartir;
+
+    TextView txPregunta;
+    TextView txSiNo;
+    TextView txPalabra1;
+    TextView txPalabra2;
+    TextView txPalabra3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        setListeners();
+        findViews();
+        generateRandomWords();
+
+        Intent intent = getIntent();
+        String preguntaObtenida = intent.getExtras().getString("pregunta");
+        txPregunta.setText(preguntaObtenida);
+
+
     }
 
     @Override
@@ -33,5 +61,74 @@ public class ResultadoActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private ArrayList generateRandomNumbers(int nnumbers,int maxnumber){
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        Random randomGenerator = new Random();
+        while (numbers.size() < nnumbers) {
+
+            int random = randomGenerator .nextInt(maxnumber);
+            if (!numbers.contains(random)) {
+                numbers.add(random);
+            }
+        }
+        return numbers;
+    }
+
+    private void generateRandomWords(){
+        ArrayList<Integer> nsino = generateRandomNumbers(1,2);
+        if(nsino.get(0)==0){
+            txSiNo.setText(getResources().getString(R.string.si));
+        }
+        else if(nsino.get(0)==1){
+            txSiNo.setText(getResources().getString(R.string.no));
+        }
+
+        ArrayList<Integer> npalabras = generateRandomNumbers(3,10);
+
+        String selector1 = "palabra"+npalabras.get(0).toString();
+        String selector2 = "palabra"+npalabras.get(1).toString();
+        String selector3 = "palabra"+npalabras.get(2).toString();
+
+        int IdPal1 = getResources().getIdentifier(selector1, "string", "es.jcolladosp.daviscope");
+        int IdPal2 = getResources().getIdentifier(selector2, "string", "es.jcolladosp.daviscope");
+        int IdPal3 = getResources().getIdentifier(selector3, "string", "es.jcolladosp.daviscope");
+
+        txPalabra1.setText(getString(IdPal1));
+        txPalabra2.setText(getString(IdPal2));
+        txPalabra3.setText(getString(IdPal3));
+    }
+
+
+    private void setListeners() {
+        volver = (Button) findViewById(R.id.btOtra);
+        volver.setOnClickListener(this);
+
+        compartir = (Button) findViewById(R.id.btCompartir);
+        compartir.setOnClickListener(this);
+
+    }
+
+    private void findViews(){
+        txPregunta = (TextView) findViewById(R.id.txPregunta);
+        txSiNo = (TextView) findViewById(R.id.txSiNo);
+        txPalabra1 = (TextView) findViewById(R.id.txPalabra1);
+        txPalabra2 = (TextView) findViewById(R.id.txPalabra2);
+        txPalabra3 = (TextView) findViewById(R.id.txPalabra3);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btOtra) {
+            onBackPressed();
+        }
+        if (v.getId() == R.id.btCompartir) {
+
+
+        }
+
     }
 }
