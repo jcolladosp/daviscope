debab959a024723b3f4bb0595a285aa553b2b312
jcolladosp <jocolsan@inf.upv.es>
2015-10-05 12:39:40 +0200

    Compartir whatsapp, seleccionar idioma, email, etc

--- app/src/main/java/es/jcolladosp/daviscope/ResultadoActivity.java
+++ app/src/main/java/es/jcolladosp/daviscope/ResultadoActivity.java
@@ -1,29 +1,46 @@
 package es.jcolladosp.daviscope;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v7.app.ActionBar;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
+
+import java.util.Locale;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 
 import es.jcolladosp.daviscope.Util.BaseActivity;
 
 public class ResultadoActivity extends BaseActivity implements View.OnClickListener {
     Button volver;
-    Button compartir;
+    ImageButton compartir;
 
     TextView txPregunta;
     TextView txSiNo;
     TextView txPalabra1;
     TextView txPalabra2;
     TextView txPalabra3;
+    AlertDialog levelDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +55,13 @@ public class ResultadoActivity extends BaseActivity implements View.OnClickListe
         String preguntaObtenida = intent.getExtras().getString("pregunta");
         txPregunta.setText(preguntaObtenida);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+            actionBar.setTitle("DaviScope");
+
+        }
 
     }
 
@@ -50,15 +74,31 @@ public class ResultadoActivity extends BaseActivity implements View.OnClickListe
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            selectLanguage();
             return true;
         }
+        if (id == R.id.ajuda) {
+            Intent send = new Intent(Intent.ACTION_SENDTO);
+            String uriText = "mailto:" + Uri.encode("jousselin.new.antique@gmail.com") +
+                    "?subject=" + Uri.encode(getResources().getString(R.string.email) + "Daviscope") +
+                    "&body=" + Uri.encode("");
+            Uri uri = Uri.parse(uriText);
+
+            send.setData(uri);
+            startActivityForResult(Intent.createChooser(send, getResources().getString(R.string.email)), 1);
+
+            return true;
+        }
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -77,13 +117,6 @@ public class ResultadoActivity extends BaseActivity implements View.OnClickListe
     }
 
     private void generateRandomWords(){
-        ArrayList<Integer> nsino = generateRandomNumbers(1,2);
-        if(nsino.get(0)==0){
-            txSiNo.setText(getResources().getString(R.string.si));
-        }
-        else if(nsino.get(0)==1){
-            txSiNo.setText(getResources().getString(R.string.no));
-        }
 
         ArrayList<Integer> npalabras = generateRandomNumbers(3,42);
 
@@ -105,18 +138,42 @@ public class ResultadoActivity extends BaseActivity implements View.OnClickListe
         volver = (Button) findViewById(R.id.btOtra);
         volver.setOnClickListener(this);
 
-        compartir = (Button) findViewById(R.id.btCompartir);
+        compartir = (ImageButton) findViewById(R.id.btCompartir);
         compartir.setOnClickListener(this);
 
     }
 
     private void findViews(){
         txPregunta = (TextView) findViewById(R.id.txPregunta);
-        txSiNo = (TextView) findViewById(R.id.txSiNo);
+
         txPalabra1 = (TextView) findViewById(R.id.txPalabra1);
         txPalabra2 = (TextView) findViewById(R.id.txPalabra2);
         txPalabra3 = (TextView) findViewById(R.id.txPalabra3);
 
+
+    }
+
+    public void onClickWhatsApp(View view) {
+
+        PackageManager pm=getPackageManager();
+        try {
+
+            Intent waIntent = new Intent(Intent.ACTION_SEND);
+            waIntent.setType("text/plain");
+            String text = "YOUR TEXT HERE";
+
+            PackageInfo info=pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+            //Check if package exists or not. If not then code
+            //in catch block will be called
+            waIntent.setPackage("com.whatsapp");
+
+            waIntent.putExtra(Intent.EXTRA_TEXT, text);
+            startActivity(Intent.createChooser(waIntent, "Share with"));
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "WhatsApp not Installed", Toast.LENGTH_SHORT)
+                    .show();
+        }
 
     }
 
@@ -126,9 +183,54 @@ public class ResultadoActivity extends BaseActivity implements View.OnClickListe
             onBackPressed();
         }
         if (v.getId() == R.id.btCompartir) {
-
+            onClickWhatsApp(v);
 
         }
 
+    }
+    private void selectLanguage(){
+
+
+// Strings to Show In Dialog with Radio Buttons
+        final CharSequence[] items = {" English "," Español "," Français "};
+
+        // Creating and Building the Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getResources().getString(R.string.action_settings));
+        builder.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+
+
+                switch(item)
+                {
+                    case 0:
+                        setLocale("en");
+                        break;
+                    case 1:
+                        setLocale("es");
+
+                        break;
+                    case 2:
+                        setLocale("fr");
+                        break;
+
+
+                }
+                levelDialog.dismiss();
+            }
+        });
+        levelDialog = builder.create();
+        levelDialog.show();
+    }
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, PreguntaActivity.class);
+        startActivity(refresh);
+        finish();
     }
 }
